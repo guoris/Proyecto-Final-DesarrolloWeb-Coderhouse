5d08bcd034507e777e7de548dd7be1e3159d9e60
Felipe <84757123+guoris@users.noreply.github.com>
2021-09-30 23:05:27 -0300

    Subiendo pagina terminada

---
+++ js/scroll.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#scroll-btn-up").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 100, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
